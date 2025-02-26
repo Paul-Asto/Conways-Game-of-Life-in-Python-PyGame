9b932284402eea155240d7edc3d23d0eb3597038
Paul A.S. Dev <astosalaspaul@gmail.com>
2025-02-26 04:06:34 -0500

    Añadiendo las clases iniciales restantes , definiendo metodos

---
+++ core/chunk.py
@@ -0,0 +1,20 @@
+from cardinal import Coord
+
+
+
+class Cell:
+    def __init__(self):
+        self.state: bool = False
+
+
+
+
+
+class Chunk:
+    
+    def __init__(self, size_y: int , size_x: int , coord: Coord):
+        self.size_y: int = size_y
+        self.size_x: int = size_x
+
+        self.coord: Coord = coord
+        self.content: dict[Coord, Cell] = {}
---
+++ core/infinite_board.py
@@ -0,0 +1,19 @@
+from core.chunk import Chunk
+from core.cardinal import Coord
+
+class InfiniteBoard:
+
+    def __init__(self, chunk_size_y: int, chunk_size_x: int):
+        self.chunk_size_y: int = chunk_size_y
+        self.chunk_size_x: int = chunk_size_x
+
+        self.content: dict[Coord, Chunk] = {}
+
+
+    def get_chunk(self, coord: Coord) -> Chunk | None: ...
+
+
+    def create_chunk(self, coord: Coord): ...
+
+
+    def gen_and_get_chunck(self, coord: Coord): ...
